6ba275cd54aba35f7683eab88afce6e30de16806
Andrea <amherman@nevis.columbia.edu>
2017-06-19 20:13:26 -0400

    reorganized base functions and classification functions into two separate files

---
+++ classification_image_gen.py
@@ -0,0 +1,164 @@
+from toydatagen.toydata_base import *
+
+def add_eight_shapes_to(array, locs, npoints = 1):
+    """ this function adds one of eight different shape types to an array each time it is called:
+    either one or two triangles, squares, horizontal lines or vertical lines. Images are labeled
+    in a 8-dimensional list."""
+
+    for i in range(npoints):
+        row=int(random.uniform(5,array.shape[0]-5))
+        col=int(random.uniform(5,array.shape[1]-5))
+        array[row, col] = random.uniform(0,4)
+        trianglocs = np.where(np.logical_and(0 < array, array < 1))
+        rectlocs = np.where(np.logical_and(array >= 1, array <2))
+        horlocs = np.where(np.logical_and(array >= 2, array < 3))
+        vertlocs = np.where(array >=3)
+
+        horxs = horlocs[0]
+        horys = horlocs[1]
+
+        vertxs = vertlocs[0]
+        vertys = vertlocs[1]
+
+        trixs = trianglocs[0]
+        triys = trianglocs[1]
+
+        rectxs = rectlocs[0]
+        rectys = rectlocs[1]
+        z = random.uniform(0,2)
+        for i in range(len(rectxs)):
+            x = rectxs[i]
+            y = rectys[i]
+            choose_rectangle(x,y,array)
+            if z < 1:
+                choose_rectangle(int(random.uniform(5, array.shape[0]-5)), int(random.uniform(5, array.shape[1]-5)), array)
+                locs.append([0,1,0,0,0,0,0,0])
+            else: locs.append([1,0,0,0,0,0,0,0])
+        for i in range(len(triys)):
+            x = trixs[i]
+            y = triys[i]
+            choose_triangle(x,y,array)
+            if z < 1:
+                choose_triangle(int(random.uniform(5, array.shape[0]-5)), int(random.uniform(5, array.shape[1]-5)), array)
+                locs.append([0,0,0,1,0,0,0,0])
+            else: locs.append([0,0,1,0,0,0,0,0])
+        for i in range(len(horxs)):
+            x = horxs[i]
+            y = horys[i]
+            choose_horizontal(x,y,array)
+            if z <1:
+                choose_horizontal(int(random.uniform(5, array.shape[0]-5)), int(random.uniform(5, array.shape[1]-5)), array)
+                locs.append([0,0,0,0,0,1,0,0])
+            else: locs.append([0,0,0,0,1,0,0,0])
+        for i in range(len(vertys)):
+            choose_vertical(vertxs[i], vertys[i], array)
+            if z <1:
+                choose_vertical(int(random.uniform(5, array.shape[0]-5)), int(random.uniform(5, array.shape[1]-5)), array)
+                locs.append([0,0,0,0,0,0,0,1])
+            else: locs.append([0,0,0,0,0,0,1,0])
+
+def add_four_shapes_to(array, locs, npoints = 1):
+    """ this function adds one of four shapes with a single multiplicity to
+    an array each time it is called, and a 1 by 4 array labeling the shape called"""
+    for i in range(npoints):
+        row=int(random.uniform(5,array.shape[0]-5))
+        col=int(random.uniform(5,array.shape[1]-5))
+        array[row, col] = random.uniform(0,4)
+        trianglocs = np.where(np.logical_and(0 < array, array < 1))
+        rectlocs = np.where(np.logical_and(array >= 1, array <2))
+        horlocs = np.where(np.logical_and(array >= 2, array < 3))
+        vertlocs = np.where(array >=3)
+
+        horxs = horlocs[0]
+        horys = horlocs[1]
+
+        vertxs = vertlocs[0]
+        vertys = vertlocs[1]
+
+        trixs = trianglocs[0]
+        triys = trianglocs[1]
+
+        rectxs = rectlocs[0]
+        rectys = rectlocs[1]
+
+        for i in range(len(rectxs)):
+            x = rectxs[i]
+            y = rectys[i]
+            choose_rectangle(x,y,array)
+            locs.append([1,0,0,0])
+        for i in range(len(triys)):
+            x = trixs[i]
+            y = triys[i]
+            choose_triangle(x,y,array)
+            locs.append([0,1,0,0])
+        for i in range(len(horxs)):
+            x = horxs[i]
+            y = horys[i]
+            choose_horizontal(x,y,array)
+            locs.append([0,0,1,0])
+        for i in range(len(vertys)):
+            choose_vertical(vertxs[i], vertys[i], array)
+            locs.append([0,0,0,1])
+
+def randomize_labels_eight():
+    """
+    This function returns an array of length 8 where only 1 element
+    is set to 1 (randomly chosen) and the rest is set to 0.
+    """
+    labels = [0,0,0,0,0,0,0,0]
+    z = random.randint(0, 7)
+    labels[z] = 1
+    return labels
+
+def randomize_labels_four():
+    """ this function returns an array of length 4, where one randomly chosen element is set to 1
+    and the rest are zeros. This is useful to debug the single-multiplicity four shape image generator. """
+
+    labels = [0,0,0,0]
+    z = random.randint(0, 3)
+    labels[z] = 1
+    return labels
+
+class image_gen_counter:
+    _counter_ = 0
+
+def make_classification_images(num_images=100,debug=0,bad_label = False, multiplicities = False):
+    """
+    This function makes a set of variable classification images. The bad_label functionality randomizes the labels assigned
+    to each image, to test training, while the debug function prints the images generated. The multiplicities corresponds
+    to the numer of classification image types generated. If false, one of four basic shapes is generated and labeled.
+    If multiplicities are included, eight basic images are generated. """
+    locations = []
+    bad_locations = []
+    images = []
+
+    for i in range(num_images):
+
+        if debug:
+            print 'Generating image',i
+
+        mat = np.zeros([28,28]).astype(np.float32)
+
+        if not multiplicities:
+            add_four_shapes_to(mat, locations)
+
+        if multiplicities:
+            add_eight_shapes_to(mat, locations)
+
+        if debug>1:
+            image(mat)
+            plt.savefig('image_%04d.png' % image_gen_counter._counter_)
+
+        mat = np.reshape(mat, (784))
+        images.append(mat)
+
+        image_gen_counter._counter_ +=1
+
+    if bad_label:
+        for loc in locations:
+            bad_locations.append(randomize_labels())
+
+    if bad_label:
+        return images, bad_locations
+    #print locations
+    return images, locations
---
+++ toydatabasic.py
@@ -0,0 +1,59 @@
+import matplotlib as mpl
+mpl.use('agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from scipy import optimize
+
+def _image(array):
+    fig,ax = plt.subplots(figsize=(28,28),facecolor='w')
+    plt.imshow(array,interpolation = 'nearest')
+    # plt.show()
+
+def _choose_triangle(x, y,array):
+    z = int(random.uniform(3,4))
+    if (z < 3 or z >=4) and array[x,y] < 10:
+        array[x,y] = 0
+        return
+    if 3 <= z < 4:
+        if 3 <= x <= len(array[0])-3:
+            if 4 <= y <= len(array[1]) - 4:
+                array[x, y] = 180 + array[x, y]
+                array[x-1:x+1, y-1] = 180 + array[x-1:x+1, y-1]
+                array[x-2:x+2, y-2] = 180 + array[x-2:x+2, y-2]
+                array[x-3:x, y-3] = 180 + array[x-3:x, y-3]
+                array[x-4:x, y-4] = 180 + array[x-4:x, y-4]
+                array[x:x+3, y-3] = 180 + array[x:x+3, y-3]
+                array[x:x+4, y-4] = 180 + array[x:x+4, y-4]
+
+
+def _choose_rectangle(x, y, array):
+    z = random.uniform(0,1)
+    if z > 1 and array[x, y] <10:
+        array[x,y] = 0
+        returm
+    if 0 < z <= 1:
+        if x+5 <= len(array[0]):
+            if y+5 <= len(array[1]):
+                array[x:x+5, y:y+5] = 0
+                points = [(x, y), (x+5, y), (x, y+5), (x+5, y+5)]
+                start_pt, end_pt = min(points), max(points)
+                array[start_pt[1]:end_pt[1]+1, start_pt[0]:end_pt[0]+1] = 180 + array[start_pt[1]:end_pt[1]+1, start_pt[0]:end_pt[0]+1]
+
+def _choose_horizontal(x, y, array):
+    z = random.uniform(0,1)
+    if 0 < z <= 1:
+         if y+5 <= len(array[1]):
+         points = [(x, y), (x, y+5)]
+              array[x, y:y+5] = 180 + array[x, y:y+5]
+              #print 1
+
+def _choose_vertical(x, y, array):
+    z = random.uniform(1,2)
+    if (z > 2 or z <1) and array[x, y] < 10:
+        array[x,y]  = 0
+        return
+    if 1 < z <= 2:
+        if x+5 <= len(array[0]):
+            points = [(x, y), (x+5, y)]
+            array[x:x+5, y] = 180 + array[x:x+5, y]
